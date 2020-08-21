9b33805557c9a637a7959f87b11d481bb1decd7a
91701 <sahana0413@gmail.com>
2020-08-21 14:15:59 +0530

    Andriod_Bookhub_App

---
+++ app/src/main/java/com/example/sahana/bookhub/model/Book.kt
@@ -0,0 +1,11 @@
+package com.example.sahana.bookhub.model
+
+data class Book(
+    val bookId: String,
+    val bookName: String,
+    val bookAuthor: String,
+    val bookRating: String,
+    val bookPrice: String,
+    val bookImage: String
+)
+
